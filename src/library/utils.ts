a9c3466559890fba886a581c0406b862c30b2914
Denis Sidletskyy <dsidletskyy@gmail.com>
2023-10-16 22:37:54 +0300

    trpc configuration

--- src/library/utils.ts
+++
@@ -1,6 +0,0 @@
-import clsx, { ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...classValues: Array<ClassValue>) {
-    return twMerge(clsx(classValues))
-}
